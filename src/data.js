d1d03148fa3cd688951cd8b1f7478ade30acf754
Sakib-Avdibasic <sakibavdibasic003@gmail.com>
2021-10-20 22:20:37 +0200

    updated fighter bios

--- src/data.js
+++ src/data.js
@@ -1,108 +1,105 @@
 const fighters = [
 	{
-		name: 'Hangman Page',
+		name: 'hangman page',
 		record: {
 			win: 17,
 			loss: 2,
 		},
 		description:
-			"I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+			'One of the hottest stars in AEW. Currently on horseback riding his way to the AEW World Championship',
 		category: 'male',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759124/item-1_yvmq2l_mc1vqy.jpg',
 	},
 	{
-		name: 'Ricky Starks',
+		name: 'ricky starks',
 		record: {
 			win: 14,
 			loss: 2,
 		},
 		description:
-			'vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats',
+			'Promising young talent. We thought of letting the talent do their own bios, but he broke the word limit',
 		category: 'male',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759829/item-2_px2uo8.jpg',
 	},
 	{
-		name: 'Lucha Bros',
+		name: 'lucha bros',
 		record: {
 			win: 15,
 			loss: 2,
 		},
 		description:
-			'ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.',
+			'The current AEW World Tag Team Champions, a title which they won with literal blood, sweat and tears',
 		category: 'tag team',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759120/item-3_ke5zss_v9xkbt.jpg',
 	},
 	{
-		name: 'Britt Baker',
+		name: 'britt baker',
 		record: {
 			win: 17,
 			loss: 4,
 		},
-		description:
-			'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,',
+		description: "Adam Cole's girlfriend",
 		category: 'female',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759823/item-5_jxdohg.jpg',
 	},
 	{
-		name: 'Young Bucks',
+		name: 'young bucks',
 		record: {
 			win: 10,
 			loss: 2,
 		},
 		description:
-			'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,',
+			'An awe-inducing and dominant team. Their in-ring skills are as good as their fashion sense is bad',
 		category: 'tag team',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759541/item-5_vplvgv.jpg',
 	},
 	{
-		name: 'Kris Statlander',
+		name: 'kris statlander',
 		record: {
 			win: 21,
 			loss: 1,
 		},
-		description:
-			'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,',
+		description: 'Alien from another galaxy',
 		category: 'female',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759122/item-6_vwuykp_neezxe.jpg',
 	},
 	{
-		name: 'The Butcher & The Blade',
+		name: 'the butcher & the blade',
 		record: {
 			win: 8,
 			loss: 2,
 		},
 		description:
-			'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,',
+			'A straight-to-the-point highly dangerous and unrelenting team, aided by The Bunny',
 		category: 'tag team',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759122/item-7_zpbr7q_j3gqcz.jpg',
 	},
 	{
-		name: 'Miro',
+		name: 'miro',
 		record: {
 			win: 16,
 			loss: 2,
 		},
 		description:
-			'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,',
+			'A killer motivated only by his God and his wife on a path of vengeance after losing his title',
 		category: 'male',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759124/item-8_gxqdjn_asfqhv.jpg',
 	},
 	{
-		name: 'Tay Conti',
+		name: 'tay conti',
 		record: {
 			win: 33,
 			loss: 5,
 		},
-		description:
-			'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,',
+		description: 'Jiu-jitsu black belt. Fun-loving when left unprovoked',
 		category: 'female',
 		photo:
 			'https://res.cloudinary.com/dkqrhdu4c/image/upload/v1634759840/item-9_kpkdah.png',
